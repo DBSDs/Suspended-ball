837beca44a96b76e79bad49c6c02d166b29b14e1
hardbra1n <64662582+DBSDs@users.noreply.github.com>
2021-01-08 16:10:30 +0800

    init

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  state = false;
+  count = 0;
+  appear = false;
+  constructor(
+    private router: Router,
+    private elementRef: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+  }
+  onclick(): void {
+    const ball = this.elementRef.nativeElement.querySelector('.state-ball');
+    if(!this.state) {
+      this.state = true;
+      this.appear = false;
+      this.count === 101 ? this.count = 0:'';
+      const ballInter = setInterval(() => {
+        if(this.count <= 100) {
+          ball.style.setProperty('--offset', this.count);
+          this.count ++;
+        } else {
+          this.state = false;
+          this.appear = true;
+          clearInterval(ballInter);
+        }
+      },10);
+    }
+  }
+}
